623eb750335da453865598a9a1ec96e236f29cde
sakun <sakun@SSSPC>
2023-05-25 02:28:05 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace WinFormsApp1 {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            menuStrip1 = new MenuStrip();
+            файлToolStripMenuItem = new ToolStripMenuItem();
+            выходToolStripMenuItem = new ToolStripMenuItem();
+            информацияToolStripMenuItem = new ToolStripMenuItem();
+            рисункиToolStripMenuItem = new ToolStripMenuItem();
+            классификацияToolStripMenuItem = new ToolStripMenuItem();
+            постоянныеЗУToolStripMenuItem = new ToolStripMenuItem();
+            оперативныкЗУToolStripMenuItem = new ToolStripMenuItem();
+            мокросхемыToolStripMenuItem = new ToolStripMenuItem();
+            диаграммыToolStripMenuItem = new ToolStripMenuItem();
+            обучениеToolStripMenuItem = new ToolStripMenuItem();
+            к565РУ7ToolStripMenuItem = new ToolStripMenuItem();
+            кР537РУ14ToolStripMenuItem = new ToolStripMenuItem();
+            к573РФ8ToolStripMenuItem = new ToolStripMenuItem();
+            кМ558РР3ToolStripMenuItem = new ToolStripMenuItem();
+            контрольToolStripMenuItem = new ToolStripMenuItem();
+            к565РУ7ToolStripMenuItem1 = new ToolStripMenuItem();
+            кР537РУ14ToolStripMenuItem1 = new ToolStripMenuItem();
+            к573РФ8ToolStripMenuItem1 = new ToolStripMenuItem();
+            кМ558РР3ToolStripMenuItem1 = new ToolStripMenuItem();
+            справкаToolStripMenuItem = new ToolStripMenuItem();
+            сокращенияToolStripMenuItem = new ToolStripMenuItem();
+            оПрограммеToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { файлToolStripMenuItem, информацияToolStripMenuItem, диаграммыToolStripMenuItem, справкаToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(624, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            файлToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { выходToolStripMenuItem });
+            файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            файлToolStripMenuItem.Size = new Size(48, 20);
+            файлToolStripMenuItem.Text = "Файл";
+            // 
+            // выходToolStripMenuItem
+            // 
+            выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            выходToolStripMenuItem.ShortcutKeys = Keys.Alt | Keys.F4;
+            выходToolStripMenuItem.Size = new Size(151, 22);
+            выходToolStripMenuItem.Text = "Выход";
+            // 
+            // информацияToolStripMenuItem
+            // 
+            информацияToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { рисункиToolStripMenuItem, классификацияToolStripMenuItem, постоянныеЗУToolStripMenuItem, оперативныкЗУToolStripMenuItem, мокросхемыToolStripMenuItem });
+            информацияToolStripMenuItem.Name = "информацияToolStripMenuItem";
+            информацияToolStripMenuItem.Size = new Size(93, 20);
+            информацияToolStripMenuItem.Text = "Информация";
+            // 
+            // рисункиToolStripMenuItem
+            // 
+            рисункиToolStripMenuItem.Name = "рисункиToolStripMenuItem";
+            рисункиToolStripMenuItem.Size = new Size(166, 22);
+            рисункиToolStripMenuItem.Text = "Рисунки";
+            // 
+            // классификацияToolStripMenuItem
+            // 
+            классификацияToolStripMenuItem.Name = "классификацияToolStripMenuItem";
+            классификацияToolStripMenuItem.Size = new Size(166, 22);
+            классификацияToolStripMenuItem.Text = "Классификация";
+            // 
+            // постоянныеЗУToolStripMenuItem
+            // 
+            постоянныеЗУToolStripMenuItem.Name = "постоянныеЗУToolStripMenuItem";
+            постоянныеЗУToolStripMenuItem.Size = new Size(166, 22);
+            постоянныеЗУToolStripMenuItem.Text = "Постоянные ЗУ";
+            // 
+            // оперативныкЗУToolStripMenuItem
+            // 
+            оперативныкЗУToolStripMenuItem.Name = "оперативныкЗУToolStripMenuItem";
+            оперативныкЗУToolStripMenuItem.Size = new Size(166, 22);
+            оперативныкЗУToolStripMenuItem.Text = "Оперативнык ЗУ";
+            // 
+            // мокросхемыToolStripMenuItem
+            // 
+            мокросхемыToolStripMenuItem.Name = "мокросхемыToolStripMenuItem";
+            мокросхемыToolStripMenuItem.Size = new Size(166, 22);
+            мокросхемыToolStripMenuItem.Text = "Мокросхемы";
+            // 
+            // диаграммыToolStripMenuItem
+            // 
+            диаграммыToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { обучениеToolStripMenuItem, контрольToolStripMenuItem });
+            диаграммыToolStripMenuItem.Name = "диаграммыToolStripMenuItem";
+            диаграммыToolStripMenuItem.Size = new Size(85, 20);
+            диаграммыToolStripMenuItem.Text = "Диаграммы";
+            // 
+            // обучениеToolStripMenuItem
+            // 
+            обучениеToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { к565РУ7ToolStripMenuItem, кР537РУ14ToolStripMenuItem, к573РФ8ToolStripMenuItem, кМ558РР3ToolStripMenuItem });
+            обучениеToolStripMenuItem.Name = "обучениеToolStripMenuItem";
+            обучениеToolStripMenuItem.Size = new Size(180, 22);
+            обучениеToolStripMenuItem.Text = "Обучение";
+            // 
+            // к565РУ7ToolStripMenuItem
+            // 
+            к565РУ7ToolStripMenuItem.Name = "к565РУ7ToolStripMenuItem";
+            к565РУ7ToolStripMenuItem.Size = new Size(132, 22);
+            к565РУ7ToolStripMenuItem.Text = "К565РУ7";
+            // 
+            // кР537РУ14ToolStripMenuItem
+            // 
+            кР537РУ14ToolStripMenuItem.Name = "кР537РУ14ToolStripMenuItem";
+            кР537РУ14ToolStripMenuItem.Size = new Size(132, 22);
+            кР537РУ14ToolStripMenuItem.Text = "КР537РУ14";
+            // 
+            // к573РФ8ToolStripMenuItem
+            // 
+            к573РФ8ToolStripMenuItem.Name = "к573РФ8ToolStripMenuItem";
+            к573РФ8ToolStripMenuItem.Size = new Size(132, 22);
+            к573РФ8ToolStripMenuItem.Text = "К573РФ8";
+            // 
+            // кМ558РР3ToolStripMenuItem
+            // 
+            кМ558РР3ToolStripMenuItem.Name = "кМ558РР3ToolStripMenuItem";
+            кМ558РР3ToolStripMenuItem.Size = new Size(132, 22);
+            кМ558РР3ToolStripMenuItem.Text = "КМ558РР3";
+            // 
+            // контрольToolStripMenuItem
+            // 
+            контрольToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { к565РУ7ToolStripMenuItem1, кР537РУ14ToolStripMenuItem1, к573РФ8ToolStripMenuItem1, кМ558РР3ToolStripMenuItem1 });
+            контрольToolStripMenuItem.Name = "контрольToolStripMenuItem";
+            контрольToolStripMenuItem.Size = new Size(180, 22);
+            контрольToolStripMenuItem.Text = "Контроль";
+            // 
+            // к565РУ7ToolStripMenuItem1
+            // 
+            к565РУ7ToolStripMenuItem1.Name = "к565РУ7ToolStripMenuItem1";
+            к565РУ7ToolStripMenuItem1.Size = new Size(180, 22);
+            к565РУ7ToolStripMenuItem1.Text = "К565РУ7";
+            к565РУ7ToolStripMenuItem1.Click += к565РУ7ToolStripMenuItem1_Click;
+            // 
+            // кР537РУ14ToolStripMenuItem1
+            // 
+            кР537РУ14ToolStripMenuItem1.Name = "кР537РУ14ToolStripMenuItem1";
+            кР537РУ14ToolStripMenuItem1.Size = new Size(180, 22);
+            кР537РУ14ToolStripMenuItem1.Text = "КР537РУ14";
+            // 
+            // к573РФ8ToolStripMenuItem1
+            // 
+            к573РФ8ToolStripMenuItem1.Name = "к573РФ8ToolStripMenuItem1";
+            к573РФ8ToolStripMenuItem1.Size = new Size(180, 22);
+            к573РФ8ToolStripMenuItem1.Text = "К573РФ8";
+            // 
+            // кМ558РР3ToolStripMenuItem1
+            // 
+            кМ558РР3ToolStripMenuItem1.Name = "кМ558РР3ToolStripMenuItem1";
+            кМ558РР3ToolStripMenuItem1.Size = new Size(180, 22);
+            кМ558РР3ToolStripMenuItem1.Text = "КМ558РР3";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            справкаToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { сокращенияToolStripMenuItem, оПрограммеToolStripMenuItem });
+            справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            справкаToolStripMenuItem.Size = new Size(65, 20);
+            справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // сокращенияToolStripMenuItem
+            // 
+            сокращенияToolStripMenuItem.Name = "сокращенияToolStripMenuItem";
+            сокращенияToolStripMenuItem.Size = new Size(149, 22);
+            сокращенияToolStripMenuItem.Text = "Сокращения";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            оПрограммеToolStripMenuItem.Size = new Size(149, 22);
+            оПрограммеToolStripMenuItem.Text = "О программе";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.main;
+            BackgroundImageLayout = ImageLayout.None;
+            ClientSize = new Size(624, 371);
+            Controls.Add(menuStrip1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "MEMDEV";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem файлToolStripMenuItem;
+        private ToolStripMenuItem информацияToolStripMenuItem;
+        private ToolStripMenuItem диаграммыToolStripMenuItem;
+        private ToolStripMenuItem обучениеToolStripMenuItem;
+        private ToolStripMenuItem к565РУ7ToolStripMenuItem;
+        private ToolStripMenuItem кР537РУ14ToolStripMenuItem;
+        private ToolStripMenuItem к573РФ8ToolStripMenuItem;
+        private ToolStripMenuItem кМ558РР3ToolStripMenuItem;
+        private ToolStripMenuItem контрольToolStripMenuItem;
+        private ToolStripMenuItem к565РУ7ToolStripMenuItem1;
+        private ToolStripMenuItem кР537РУ14ToolStripMenuItem1;
+        private ToolStripMenuItem к573РФ8ToolStripMenuItem1;
+        private ToolStripMenuItem кМ558РР3ToolStripMenuItem1;
+        private ToolStripMenuItem справкаToolStripMenuItem;
+        private ToolStripMenuItem выходToolStripMenuItem;
+        private ToolStripMenuItem рисункиToolStripMenuItem;
+        private ToolStripMenuItem классификацияToolStripMenuItem;
+        private ToolStripMenuItem постоянныеЗУToolStripMenuItem;
+        private ToolStripMenuItem оперативныкЗУToolStripMenuItem;
+        private ToolStripMenuItem мокросхемыToolStripMenuItem;
+        private ToolStripMenuItem сокращенияToolStripMenuItem;
+        private ToolStripMenuItem оПрограммеToolStripMenuItem;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,12 @@
+namespace WinFormsApp1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void ъ565аг7ToolStripMenuItem1_Click(object sender, EventArgs e) {
+            First first = new First();
+            first.ShowDialog();
+        }
+    }
+}
